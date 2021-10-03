32fd1249a4378be07ca8f508073306b7c2066633
Mark Anthony Scheker <mscheker@ecegrid-thin2.ecn.purdue.edu>
2021-10-03 04:27:47 -0400

    tictactoe

---
+++ tictaktoe/tictactoe.c
@@ -0,0 +1,12 @@
+
+#include <tictactoe.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{//This file just calls the instruction function that will start the game
+  instructions();
+  return 0;
+}
+
+
---
+++ tictaktoe/tictactoe.h
@@ -0,0 +1,10 @@
+int instructions();
+void selection();
+void twoplayer();
+void player();
+void visual();
+void solo();
+int selectnum();
+int startchick();
+void circuischicken();
+void startcat();
